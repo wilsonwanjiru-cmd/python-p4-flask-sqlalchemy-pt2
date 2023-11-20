c4ae231f6eb15203846b0e2600b5a83ddfdb896e
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-11-20 14:33:31 +0300

    Flask-SQLAlchemy, Part 2

--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
-
 from app import app
 from models import db, Owner, Pet
+
+fake = Faker()
+
+with app.app_context():
+    # No need to initialize Flask-SQLAlchemy again, as it's already initialized in app.py
+
+    Pet.query.delete()
+    Owner.query.delete()
+
+    owners = []
+    for n in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
+
+    db.session.add_all(owners)
+
+    pets = []
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    for n in range(100):
+        pet = Pet(name=fake.first_name(), species=rc(species), owner=rc(owners))
+        pets.append(pet)
+
+    db.session.add_all(pets)
+    db.session.commit()
